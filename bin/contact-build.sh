3073d1ed1091a699dcf9e7d0ee3b800ba7025022
Olivia <oliviatmanalastas@gmail.com>
2022-11-11 13:10:19 -0500

    edits

--- bin/contact-build.sh
+++ bin/contact-build.sh
@@ -2,7 +2,7 @@
 # exit on error
 set -o errexit
 
-`bundle install`
+bundle install
 # bundle exec rake assets:precompile # These lines are commented out because we have an API only app
 # bundle exec rake assets:clean
 bundle exec rake db:migrate 
