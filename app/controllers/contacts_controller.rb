f6bce20e5efcb1866bbcbb33ed1a2bf45a48e05e
Olivia <oliviatmanalastas@gmail.com>
2022-11-11 09:59:06 -0500

    create action created

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,4 +4,15 @@ class ContactsController < ApplicationController
       render json: Contact.all
    end
 
+   def create
+      contact = Contact.create(contact_params)
+      render json: contact, status: :created
+   end
+
+   private
+
+   def contact_params
+      params permit(:first_name, :last_name, :email, :message)
+   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   get 'contacts', to: 'contacts#index'
+  post 'contacts', to: 'contacts#create'
 end
